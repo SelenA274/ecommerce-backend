6d74f8a9f81332d1793af4beffb99d4b9267f274
selen amasha <selenamasha6@gmail.com>
2026-03-07 20:20:01 +0200

    refactor auth - move business logic to auth.service.js

--- src/features/auth/auth.controller.js
+++ src/features/auth/auth.controller.js
@@ -1,33 +1,21 @@
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
-import crypto from "crypto"
 import dotenv from "dotenv"
-import { User } from "../users/user.model.js"
-import { sendVerificationEmail } from "../../shared/utils/sendVerificationEmail.js"
-import { sendResetPasswordEmail } from "../../shared/utils/sendResetPasswordEmail.js"
+import { 
+  registerService, 
+  loginService, 
+  forgotPasswordService, 
+  resetPasswordService, 
+  adminLoginService, 
+  adminVerify2faService, 
+  verifyEmailService, 
+  meService 
+} from "./auth.service.js"
 
 dotenv.config()
-
-function generateToken() {
-    return crypto.randomBytes(32).toString("hex")
-  }
-
-function generateCode() {
-    return Math.floor(100000 + Math.random() * 900000).toString();
-}
 
 export const register = async (req, res) => {
     try {
         const { name, email, password } = req.body
-        const code = generateCode()
-        const user = await User.create({
-            name,
-            email,
-            password,
-            verificationToken: code
-        })
-        await sendVerificationEmail(email, code)
-
+        const user = await registerService({name, email, password})
         res.status(200).json({
             status: 200,
             message: "Verification code sent",
@@ -58,18 +46,7 @@ export const register = async (req, res) => {
 export async function verifyEmail(req, res) {
   try {
       const { token } = req.params  
-      
-      const user = await User.findOne({ verificationToken: token })
-      if (!user) {
-          return res.status(404).json({ 
-              message: "Invalid or expired token" 
-          })
-      }
-
-      user.isVerified = true
-      user.verificationToken = null
-      await user.save()
-      
+      await verifyEmailService({ token })
       res.status(200).json({ 
           message: "Email verified successfully" 
       })
@@ -83,33 +60,7 @@ export async function verifyEmail(req, res) {
 export const login = async (req, res) => {
     try {
         const { email, password } = req.body
-// مششكله!!!!!!!!!!!!!!!!!!!!!!!!!!
-        const user = await User.findOne({ email }).select("+password +isVerified")
-        if (!user) return res.status(400).json({
-            status: 400,
-            message: "Invalid credentials",
-            data: null
-        })
-
-        const match = await bcrypt.compare(password, user.password)
-        if (!match) return res.status(400).json({
-            status: 400,
-            message: "Invalid credentials",
-            data: null
-        })
-
-        if (!user.isVerified) return res.status(400).json({
-            status: 400,
-            message: "User is not verified",
-            data: null
-        })
-
-        const token = jwt.sign(
-            { userId: user._id },
-            process.env.JWT_SECRET,
-            { expiresIn: "30m" }
-        )
-
+        const token = await loginService({email, password})
         res.status(200).json({
             status: 200,
             message: "Login successfully",
@@ -129,24 +80,7 @@ export const login = async (req, res) => {
 export const forgotPassword = async (req, res) => {
     try {
       const { email } = req.body
-      const user = await User.findOne({ email })
-      if (!user) {
-        return res.status(200).json({
-          status: 200,
-          message: "If the email exists, a reset link was sent",// i dont like this 
-          data: null,
-        })
-      }
-  
-      const token = generateToken()
-      const expiry = new Date(Date.now() + 60 * 60 * 1000) // 1h
-  
-      user.resetPasswordToken = token
-      user.resetPasswordExpiry = expiry
-      await user.save()
-  
-      await sendResetPasswordEmail(email, token)
-  
+      await forgotPasswordService({email})
       return res.status(200).json({
         status: 200,
         message: "Reset password email sent",
@@ -167,25 +101,7 @@ export const forgotPassword = async (req, res) => {
     try {
       const { token } = req.params
       const { password } = req.body
-  
-      const user = await User.findOne({
-        resetPasswordToken: token,
-        resetPasswordExpiry: { $gt: new Date() },
-      }).select("+password +resetPasswordToken +resetPasswordExpiry")
-  
-      if (!user) {
-        return res.status(400).json({
-          status: 400,
-          message: "Invalid or expired token",
-          data: null,
-        })
-      }
-  
-      user.password = password 
-      user.resetPasswordToken = null
-      user.resetPasswordExpiry = null
-      await user.save()
-  
+      await resetPasswordService({token, password})
       return res.status(200).json({
         status: 200,
         message: "Password reset successfully",
@@ -205,40 +121,7 @@ export const forgotPassword = async (req, res) => {
   export const adminLogin = async (req, res) => {
     try {
       const { email, password } = req.body
-      const user = await User.findOne({ email }).select("+password +isVerified")
-      if (!user) {
-        return res.status(400).json({ 
-            status: 400, 
-            message: "Invalid credentials", 
-            data: null 
-        })
-      }
-  
-      const match = await bcrypt.compare(password, user.password)
-      if (!match) {
-        return res.status(400).json({ 
-            status: 400, 
-            message: "Invalid credentials", 
-            data: null 
-
-        })
-      }
-  
-      if (!user.isVerified) {
-        return res.status(400).json({ 
-            status: 400, 
-            message: "User is not verified", 
-            data: null 
-
-        })
-      }
-      const code = generateCode()
-      user.twoFactorCode = code
-      user.twoFactorExpiry = new Date(Date.now() + 10 * 60 * 1000) // 10m
-      await user.save()
-  
-      await sendVerificationEmail(user.email, code)
-  
+      await adminLoginService({email, password})
       return res.status(200).json({
         status: 200,
         message: "2FA code sent",
@@ -257,32 +140,7 @@ export const forgotPassword = async (req, res) => {
   export const adminVerify2fa = async (req, res) => {
     try {
       const { email, code } = req.body
-  
-      const user = await User.findOne({
-        email,
-        role: "admin",
-        twoFactorCode: code,
-        twoFactorExpiry: { $gt: new Date() },
-      })
-  
-      if (!user) {
-        return res.status(400).json({
-          status: 400,
-          message: "Invalid or expired 2FA code",
-          data: null,
-        })
-      }
-  
-      user.twoFactorCode = null
-      user.twoFactorExpiry = null
-      await user.save()
-  
-      const token = jwt.sign(
-        { userId: user._id, role: user.role },
-        process.env.JWT_SECRET,
-        { expiresIn: process.env.JWT_EXPIRE || "1d" }
-      )
-  
+      const token = await adminVerify2faService({email, code})
       return res.status(200).json({
         status: 200,
         message: "Admin login successfully",
@@ -318,11 +176,7 @@ export const forgotPassword = async (req, res) => {
 
   export const me = async (req, res) => {
     try {
-      const user = await User.findById(req.user.id).select("-password")
-      if (!user) {
-        return res.status(404).json({ status: 404, message: "User not found", data: null })
-      }
-  
+      const user = await meService({ userId: req.user.id })
       return res.status(200).json({
         status: 200,
         message: "User fetched successfully",
---
+++ src/features/auth/auth.service.js
@@ -0,0 +1,164 @@
+import { User } from "../users/user.model.js"
+import { sendVerificationEmail } from "../../shared/utils/sendVerificationEmail.js"
+import { sendResetPasswordEmail } from "../../shared/utils/sendResetPasswordEmail.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+import crypto from "crypto"
+
+dotenv.config()
+
+
+function generateCode() {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+}
+
+function generateToken() {
+    return crypto.randomBytes(32).toString("hex")
+}
+
+export const registerService = async ({ name, email, password }) => {
+    const code = generateCode()
+    const user = await User.create({
+        name,
+        email,
+        password,
+        verificationToken: code
+    })
+    await sendVerificationEmail(email, code)
+    return user
+}
+
+export const loginService = async ({ email, password }) => {
+    const user = await User.findOne({ email }).select("+password +isVerified")
+    if (!user) throw {
+        status: 400,
+        message: "Invalid credentials", 
+    }
+    const match = await bcrypt.compare(password, user.password)
+        if (!match) throw {
+            status: 400,
+            message: "Invalid credentials"
+        }
+        if (!user.isVerified) throw {
+            status: 400,
+            message: "User is not verified"
+        }
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "30m" }
+        )
+    return token
+}
+
+export const forgotPasswordService = async ({ email }) => {
+    const user = await User.findOne({ email })
+    if (!user) return null
+      
+      const token = generateToken()
+      const expiry = new Date(Date.now() + 60 * 60 * 1000) // 1h
+      
+      user.resetPasswordToken = token
+      user.resetPasswordExpiry = expiry
+      await user.save()
+
+      await sendResetPasswordEmail(email, token)
+      return true
+}
+
+export const resetPasswordService = async ({ token, password }) => {
+    const user = await User.findOne({
+        resetPasswordToken: token,
+        resetPasswordExpiry: { $gt: new Date() },
+    }).select("+password +resetPasswordToken +resetPasswordExpiry")
+
+    if (!user) {
+       throw {
+          status: 400,
+          message: "Invalid or expired token", 
+        }
+      }
+
+    user.password = password
+    user.resetPasswordToken = null
+    user.resetPasswordExpiry = null
+    await user.save()
+    return true
+}
+
+export const adminLoginService = async ({ email, password }) => {
+    const user = await User.findOne({ email }).select("+password +isVerified")
+    
+    if (!user) throw { 
+            status: 400, 
+            message: "Invalid credentials",     
+        }
+    
+      const match = await bcrypt.compare(password, user.password)
+      if (!match) throw { 
+        status: 400, 
+        message: "Invalid credentials"   
+    }
+
+    if (!user.isVerified) throw { 
+            status: 400, 
+            message: "User is not verified",
+      }
+
+    const code = generateCode()
+    user.twoFactorCode = code
+    user.twoFactorExpiry = new Date(Date.now() + 10 * 60 * 1000) // 10m
+    await user.save()
+
+    await sendVerificationEmail(user.email, code)
+    return true 
+}
+
+export const adminVerify2faService  = async ({ email, code }) => {
+    const user = await User.findOne({
+        email,
+        role: "admin",
+        twoFactorCode: code,
+        twoFactorExpiry: { $gt: new Date() },
+      })
+  
+      if (!user) throw {
+          status: 400,
+          message: "Invalid or expired 2FA code"
+      }
+  
+      user.twoFactorCode = null
+      user.twoFactorExpiry = null
+      await user.save()
+  
+      const token = jwt.sign(
+        { userId: user._id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXPIRE || "1d" }
+    )
+    return token 
+}
+
+export const verifyEmailService   = async ({ token }) => {
+    const user = await User.findOne({ verificationToken: token })
+    if (!user) throw { 
+        status: 404,
+        message: "Invalid or expired token" 
+    }
+    user.isVerified = true
+    user.verificationToken = null
+    await user.save()
+    return true
+}
+
+
+export const meService = async ({ userId }) => {
+    const user = await User.findById(userId).select("-password")
+    if (!user) throw { 
+        status: 404, 
+        message: "User not found", 
+        data: null 
+    }
+    return user
+}
